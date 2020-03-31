6d4ae9aad016e556a255a32e3e385b5bc7feb23e
gmaijoe <gmai95h@gmail.com>
2020-03-30 17:00:49 -0700

    allow hyperlocal requests

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -290,7 +290,7 @@ function Submit(props) {
             setDueDate(e.target.value); // YYYY-MM-DD
           }}
           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          type="date"
+          type="datetime-local"
         />
         <div className="mt-4"></div>
         {cartJSX()}
